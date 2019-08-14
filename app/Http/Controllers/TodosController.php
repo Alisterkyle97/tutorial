097fe6fc9d4e42fca8ecafb81abfede8970e84a4
Alister Kyle (1000030957) <KYLEAL1@student.op.ac.nz>
2019-08-14 14:26:39 +1200

    Second commit for tutorial

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -13,7 +13,10 @@ class TodosController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::orderBy('created_at','desc')->paginate(8);
+        return view('todos.index',[
+            'todos' => $todos,
+        ]);
     }
 
     /**
@@ -65,7 +68,10 @@ class TodosController extends Controller
      */
     public function show($id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+        return view('todos.show',[
+            'todo' => $todo,
+        ]);
     }
 
     /**
@@ -88,8 +94,28 @@ class TodosController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+  //validation rules
+  $rules = [
+    'title' => "required|string|unique:todos,title,{$id}|min:2|max:191", //Using double quotes
+    'body'  => 'required|string|min:5|max:1000',
+];
+//custom validation error messages
+$messages = [
+    'title.unique' => 'Todo title should be unique',
+];
+//First Validate the form data
+$request->validate($rules,$messages);
+//Update the Todo
+$todo        = Todo::findOrFail($id);
+$todo->title = $request->title;
+$todo->body  = $request->body;
+$todo->save(); //Can be used for both creating and updating
+//Redirect to a specified route with flash message.
+return redirect()
+    ->route('todos.show',$id)
+    ->with('status','Updated the selected Todo!');
+}
+
 
     /**
      * Remove the specified resource from storage.
@@ -97,8 +123,15 @@ class TodosController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
     public function destroy($id)
     {
-        //
+            //Delete the Todo
+    $todo = Todo::findOrFail($id);
+    $todo->delete();
+    //Redirect to a specified route with flash message.
+    return redirect()
+        ->route('todos.index')
+        ->with('status','Deleted the selected Todo!');
     }
 }
